0fc7c1f80c6f43c0b4c29066dad65e4bf516eed4
Federico Pacheco <fpacheco@fi.uba.ar>
2022-05-01 21:18:20 -0300

    Intentando agregar nginx

---
+++ app/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+const TIMEOUT = 5000;
+
+const id = Math.floor(Math.random() * 100);
+
+app.get("/", (req, res) => {
+    res.status(200).send(id + " - ping");
+});
+
+app.get("/timeout", (req, res) => {
+    setTimeout(() => {
+        res.status(200).send(id + " - timeout");
+    }, TIMEOUT);
+});
+
+app.get("/heavy", (req, res) => {
+    let start = new Date();
+
+    while (new Date() - start < TIMEOUT);
+
+    res.status(200).send(id + " - heavy");
+});
+
+app.listen(PORT, () => {
+    console.log("Escuchando en puerto", PORT);
+});
