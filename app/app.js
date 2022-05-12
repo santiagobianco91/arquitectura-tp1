e94c1430502a4e6220eebc76afa909e2e139f6d6
santiago.bianco91 <santiago.bianco91@gmail.com>
2022-05-12 16:00:55 -0300

    buscamos sincronia en los endpoints de nodeapp

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+var request1 = require('sync-request');
 
 const app = express();
 
@@ -19,15 +20,19 @@ app.get("/ping", (req, res) => {
 });
 
 app.get("/proxy_9090", (req, res_major) => {
-  request("http://1c22-tp-1_bbox_1:9090", { json: true }, (err, res, body) => {
-    if (err) {
-      console.log("ERROR BBOX: " + err);
-      return res_major
-        .status(res ? res.statusCode : 500)
-        .send("ERROR BBOX: " + err);
-    }
-    res_major.status(res.statusCode).send(body);
-  });
+  //var res = request1('GET', 'http://1c22-tp-1_bbox_1:9090');
+  var res = request1('GET', 'http://localhost:3010/timeout');
+  console.log(res.statusCode);
+  return res_major.status(res.statusCode).send(res.body);
+  // request("http://1c22-tp-1_bbox_1:9090", { json: true }, (err, res, body) => {
+  //   if (err) {
+  //     console.log("ERROR BBOX: " + err);
+  //     return res_major
+  //       .status(res ? res.statusCode : 500)
+  //       .send("ERROR BBOX: " + err);
+  //   }
+  //   res_major.status(res.statusCode).send(body);
+  // });
 });
 
 app.get("/proxy_9091", (req, res_major) => {
@@ -42,10 +47,9 @@ app.get("/proxy_9091", (req, res_major) => {
   });
 });
 
-app.get("/timeout", (req, res) => {
-  setTimeout(() => {
-    res.status(200).send(id + " - timeout");
-  }, TIMEOUT);
+app.get("/timeout", async (req, res)  => {
+  await new Promise(r => setTimeout(r, 5000));
+  res.status(200).send("timeout 5 segundos");
 });
 
 app.get("/heavy", (req, res) => {
